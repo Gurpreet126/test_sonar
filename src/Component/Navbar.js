cc63000ec200fbc9da78f47de4028a2b03a18344
Gurpreet126 <sfs.gurpreet22@gmail.com>
2024-11-19 15:59:25 +0530

    resolve sonar cloud errors

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -69,6 +69,7 @@ export default function Navbar() {
             }}
             aria-label="Menu"
             aria-pressed="false"
+            aria-role="button"
           >
             <img src={menu} alt="menu" />
           </div>
